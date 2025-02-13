127ecb2d8b39955067f2316e207b7a244fc92f40
Lokotok <lokot.ok911@mail.ru>
2025-02-13 18:42:54 +0300

    задача 3_модуль2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
+  read(arg1, arg2) {
+    this.arg1 = arg1;
+    this.arg2 = arg2;
+  },
+
+  sum() {
+    return this.arg1 + this.arg2;
+  },
+
+  mul() {
+    return this.arg1 * this.arg2;
+  },
 };
+
+
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально

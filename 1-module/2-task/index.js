ac202706dca270dfd54407e3423e7fd71ad4df72
Lokotok <lokot.ok911@mail.ru>
2025-02-13 10:14:23 +0300

    убрали тeрнарный оператоp

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return (name && !name.includes(" ") && name.length > 3) ? true : false;
+  return Boolean(name) && !name.includes(" ") && name.length > 3;
 }
 
 function sayHello() {

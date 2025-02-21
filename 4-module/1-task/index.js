2a4d1a11e0c0a6585e155f84d0279c554f229361
Lokotok <lokot.ok911@mail.ru>
2025-02-21 12:55:40 +0300

    задача1_модуль4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+  const saved = document.querySelector('body');
+  saved.append(ul);
+  for (let i = 0; i < friends.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = friends[i].firstName + ' ' + friends[i].lastName;
+    const savedli = document.querySelector('ul');
+    savedli.append(li);
+  }
+  return ul;
 }

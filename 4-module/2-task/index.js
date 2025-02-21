c260200c6bc1a4f129e0fcd262eb562dd17be37b
Lokotok <lokot.ok911@mail.ru>
2025-02-21 17:40:11 +0300

    задача2_модуль4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let elem;
+  for (let i = 0; table.rows[i]; i++) {
+    elem = table.rows[i].cells[i];
+    elem.style.background = 'red';
+  }
 }

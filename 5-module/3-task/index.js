19fd030c4a253290be5acd09c7ed303be72d4913
Lokotok <lokot.ok911@mail.ru>
2025-02-25 16:53:00 +0300

    задача3_модуль5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  const buttonRight = document.querySelector('.carousel__arrow_right');
+  const buttonLeft = document.querySelector('.carousel__arrow_left');
+  const slideList = document.querySelectorAll('.carousel__slide'); 
+  const scroll = document.querySelector('.carousel__inner');
+  let index = 0;
+  buttonLeft.style.display = 'none';
+
+  function showSlide() {
+    let sliderWidth = slideList[index].offsetWidth;
+    scroll.style.transform = `translateX(-${sliderWidth*index}px)`;
+  }
+
+  buttonRight.addEventListener ('click', () => {
+    index++;
+    buttonLeft.style.display = '';
+    if (index == slideList.length - 1) {
+      buttonRight.style.display = 'none';
+    }
+    showSlide();
+  });
+
+  buttonLeft.addEventListener ('click', () => {
+      index--;
+      if (index == 0) {
+        buttonLeft.style.display = 'none';
+      }
+      buttonRight.style.display = '';
+      showSlide();
+   });
 }

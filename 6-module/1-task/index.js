616bc18dfee80b26353087ea6051f8dd4cf97b6a
Lokotok <lokot.ok911@mail.ru>
2025-03-07 17:56:08 +0300

    задача1_модуль6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,43 @@
  *      }
  *
  */
+
 export default class UserTable {
+  elem = null;
+  #rows = [];
+
   constructor(rows) {
+    this.#rows = rows || this.#rows;
+    this.elem = this.#renderTable();
+    this.#onButtonClick();
   }
+
+  #renderTable() {
+    const table = document.createElement('table');
+    table.innerHTML =  `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody data-name="Container">
+      ${this.#rows
+        .map(e => `<tr class="close">${Object.values(e).map(el => `<td>${el}</td>`)
+        .join('\n') + `<td><button class="remove">X</button></td>`}</tr>`)
+        .join('\n')}
+    </tbody>`;
+    return table;
+  }
+
+  #onButtonClick() {
+    this.elem.addEventListener('click', (event) => {
+      if (event.target.classList.contains('remove')) {
+        event.target.closest('tr').remove();
+      };
+    });
+  };
 }
